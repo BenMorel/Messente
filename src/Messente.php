a25f48a2bcf581f7d06b51f5803858a85b2b6ff5
Benjamin Morel <benjamin.morel@gmail.com>
2017-07-30 14:54:32 +0200

    Implement backup API

--- src/Messente.php
+++ src/Messente.php
@@ -11,23 +11,38 @@ use GuzzleHttp\RequestOptions;
  */
 class Messente
 {
-    const PRIMARY_API_ENDPOINT = 'https://api2.messente.com/send_sms/';
-    const BACKUP_API_ENDPOINT  = 'https://api3.messente.com/send_sms/';
+    const PRIMARY_API = 'https://api2.messente.com';
+    const BACKUP_API  = 'https://api3.messente.com';
+
+    const SEND_SMS_ENDPOINT = '/send_sms/';
 
     /**
+     * The Guzzle HTTP client.
+     *
      * @var Client
      */
     private $client;
 
     /**
+     * The API username.
+     *
      * @var string
      */
     private $username;
 
     /**
+     * The API password.
+     *
      * @var string
      */
     private $password;
+
+    /**
+     * Whether to use the backup API.
+     *
+     * @var bool
+     */
+    private $useBackupApi = false;
 
     /**
      * Class constructor.
@@ -41,6 +56,38 @@ class Messente
 
         $this->username = $username;
         $this->password = $password;
+    }
+
+    /**
+     * Sets whether to use the backup API.
+     *
+     * This can be used when the primary API is down for any reason.
+     *
+     * @param bool $useBackupApi True to use the backup API, false to use the primary API (default).
+     *
+     * @return void
+     */
+    public function setUseBackupApi(bool $useBackupApi)
+    {
+        $this->useBackupApi = $useBackupApi;
+    }
+
+    /**
+     * @return bool
+     */
+    public function getUseBackupApi() : bool
+    {
+        return $this->useBackupApi;
+    }
+
+    /**
+     * @param string $endpoint
+     *
+     * @return string
+     */
+    private function getApiUrl(string $endpoint) : string
+    {
+        return ($this->useBackupApi ? self::BACKUP_API : self::PRIMARY_API) . $endpoint;
     }
 
     /**
@@ -66,8 +113,10 @@ class Messente
             $parameters['from'] = $from;
         }
 
+        $url = $this->getApiUrl(self::SEND_SMS_ENDPOINT);
+
         try {
-            $response = $this->client->post(self::PRIMARY_API_ENDPOINT, [
+            $response = $this->client->post($url, [
                 RequestOptions::QUERY => $parameters
             ]);
         } catch (RequestException $e) {
